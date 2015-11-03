2d7a9cf2e85ff51af83952026ea487baaeed2396
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-11-03 10:41:27 +0000

    Added pip2pi dependency and awscli dependency.

--- package-management.rb
+++ package-management.rb
@@ -1,37 +1,24 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class PackageManagement < Formula
-  desc ""
+  desc "Creates a repository in s3 based on a requirements"
   homepage "https://github.com/drewsonne/package-management"
-  url "https://github.com/drewsonne/package-management/archive/0.1.0.tar.gz"
-  version "0.1.0"
+  url "https://github.com/drewsonne/package-management/archive/0.1.1.tar.gz"
+  version "0.1.1"
   sha256 "146099ce6dfc07324c4f279e18e0fbb5667ed419d9e8152661f3bab116db9400"
+  depends_on "awscli"
 
-  def install
-    bin.install "pip2s3"
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # system "make", "install" # if this fails, try separate make/make install steps
+  resource "pip2pi" do
+    url "https://pypi.python.org/packages/source/p/pip2pi/pip2pi-0.6.8.tar.gz"
+    sha256 "01102883670742bdc2d3e6cc572fbb7263b3396699d2368d575dae9c6b4cbb87"
   end
 
-  # test do
-  #   # `test do` will create, run in and delete a temporary directory.
-  #   #
-  #   # This test will fail and we won't accept that! It's enough to just replace
-  #   # "false" with the main program this formula installs, but it'd be nice if you
-  #   # were more thorough. Run the test with `brew test package-management`. Options passed
-  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-  #   #
-  #   # The installed folder is not in the path, so use the entire path to any
-  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
-  #   system "false"
-  # end
+  def install
+    resource("pip2pi").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
+    bin.install "pip2s3"
+  end
+
+  test do
+    system "#{bin}/pip2s3", "-h"
+  end
+
 end
