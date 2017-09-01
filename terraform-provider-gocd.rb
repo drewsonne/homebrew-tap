43d15dbddecfa1572e9a78fd7c7f1e617d0bb143
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-01 16:42:04 +0100

    Added dependency

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -2,8 +2,10 @@ class TerraformProviderGocd < Formula
   desc "Terraform Provider to manage resources in GoCD Server"
   homepage "https://github.com/drewsonne/terraform-provider-gocd"
   url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.0.11-rc2/terraform-provider-gocd-0.0.11-rc2-darwin-amd64.zip"
-  version "0.0.11-rc2"
+  version "0.0.11-rc3"
   sha256 "4490b7de3a538e613edfe9fe10dbba63dfb1d7930d7a42c36fd7ff34b3c02269"
+
+  depends_on "drewsonne/devops/tf-install-provider"
 
   def install
     bin.install "terraform-provider-gocd"
