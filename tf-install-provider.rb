4963f491989d795fcc4e39c71ef0ca2ef6152ca6
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-01 16:58:06 +0100

    Set correct hash

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -3,7 +3,7 @@ class TfInstallProvider < Formula
   homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
   url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/3ae1d713cb75104788b4186a2906c47744cf17e9.zip"
   version "1.0.3"
-  sha256 "50408ecb308b8932ef0526db5dad8dd153b1fe917631966dcc8df18fce38b3e8"
+  sha256 "596cf6ccc357e57370cd4b8388b28c9db0ea8079cd22d3b5223a4588c25ab816"
 
   # depends_on "cmake" => :build
 
