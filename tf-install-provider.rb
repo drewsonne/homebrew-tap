33c502bdffcc71d67896bb83cd692fac631ae8bc
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-01 16:57:31 +0100

    Moved tf-install-provider to gist

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -1,8 +1,8 @@
 class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
-  homepage "https://github.com/drewsonne/homebrew-devops/blob/master/terraform-provider-gocd.rb"
-  url "https://github.com/drewsonne/homebrew-devops/archive/master.zip"
-  version "1.0.2"
+  homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
+  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/3ae1d713cb75104788b4186a2906c47744cf17e9.zip"
+  version "1.0.3"
   sha256 "50408ecb308b8932ef0526db5dad8dd153b1fe917631966dcc8df18fce38b3e8"
 
   # depends_on "cmake" => :build
