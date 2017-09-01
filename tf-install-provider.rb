fac5f63394ccc7ca3034b3f6a00768cb0a7cddd7
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-01 17:20:10 +0100

    Updated download

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -1,7 +1,7 @@
 class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
   homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
-  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/3ae1d713cb75104788b4186a2906c47744cf17e9.zip"
+  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/451cf815d2d4eaaf2e9b358e765fc16b357620a8.zip"
   version "1.0.3"
   sha256 "596cf6ccc357e57370cd4b8388b28c9db0ea8079cd22d3b5223a4588c25ab816"
 
