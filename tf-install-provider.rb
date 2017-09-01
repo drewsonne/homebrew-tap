408b5adc8e387d884a5dcce4f84e27fe817cf641
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-01 16:47:55 +0100

    Updated install helper

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -2,7 +2,7 @@ class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
   homepage "https://github.com/drewsonne/homebrew-devops/blob/master/terraform-provider-gocd.rb"
   url "https://github.com/drewsonne/homebrew-devops/archive/master.zip"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "daffebcd6664830aec4b1a276c7a16913fda816c0e282ad1bdb3b9093b1327ce"
 
   # depends_on "cmake" => :build
