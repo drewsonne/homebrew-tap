509b2b8b93e0f83b3aef0a79e74febe7a554ba27
Drew J. Sonne <drew.sonne@beamly.com>
2017-09-04 14:24:21 +0100

    Added formula for artifactory installer

---
+++ terraform-provider-artifactory.rb
@@ -0,0 +1,55 @@
+require "language/go"
+
+class TerraformProviderArtifactory < Formula
+  desc "Manage Artifactory with Terraform"
+  homepage "https://github.com/webdevwilson/terraform-provider-artifactory"
+  version "1.0"
+  sha256 "4e684196d0c53d8278e99d38bd41591120aad631a1a6b9e65d4492e6cf31cc75"
+
+  depends_on "go" => :build
+  depends_on "terraform"
+  depends_on "drewsonne/devops/tf-install-provider"
+
+  url "https://github.com/webdevwilson/terraform-provider-artifactory.git",
+    :revision => "a503e6f0d3656d0120be27b410f8cbaca88fa32e"
+
+  head "https://github.com/webdevwilson/terraform-provider-artifactory.git",
+   :shallow => false
+
+  go_resource "github.com/hashicorp/terraform" do
+    url "https://github.com/hashicorp/terraform.git",
+      :revision => "3db35024878484508795a3eddb2fda29f83eb5d9"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+   
+    dir = buildpath/"src/github.com/webdevwilson/terraform-provider-artifactory"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/hashicorp/terraform" do
+      system "go", "install"
+    end
+
+    cd dir do
+      system "go build"
+      bin.install "terraform-provider-artifactory"
+    end
+
+  end
+
+  test do
+    system "#{bin}/terraform-provider-gocd; [[ $? -eq 1 ]]"
+  end
+
+  def caveats
+    """
+After installing, you will need to copy the provider to a place terraform can find it:
+  tf-install-provider artifactory
+
+"""
+  end
+
+end
