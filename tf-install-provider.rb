862532d5e3933ac4ad4fee8c82a868f8452c0148
Drew J. Sonne <drew.sonne@beamly.com>
2017-09-06 09:31:07 +0100

    Updated tf-install-provider

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -1,11 +1,9 @@
 class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
   homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
-  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/0e17b7878a2479b7cb178d39186e5940dd57c385.zip"
-  version "1.0.6"
+  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/de33c821535be3ac0b82c7b795314f314abdc1d8.zip"
+  version "1.0.7"
   sha256 "05821808c29605f794b3d4ea88e5f799ff2f67a48c42523bfea945ebe44be769"
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "tf-install-provider"
