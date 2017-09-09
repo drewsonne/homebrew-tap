7e341775702bcfa0802d563598206bf7e852226d
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-09 11:23:03 +0100

    go-gocd version 0.3.18

--- go-gocd.rb
+++ go-gocd.rb
@@ -1,9 +1,9 @@
 class GoGocd < Formula
   desc ""
   homepage "https://github.com/drewsonne/gocdsdk"
-  url "https://github.com/drewsonne/go-gocd/releases/download/0.3.17/gocd-0.3.17-darwin-x86_64.zip"
-  version "0.3.17"
-  sha256 "721760922bf4e6be8fc9e575015a6ffc84a45904c659889d31444a1b0f600125"
+  url "https://github.com/drewsonne/go-gocd/releases/download/0.3.18/gocd-0.3.18-darwin-x86_64.zip"
+  version "0.3.18"
+  sha256 "04648f54f0c2d01680f0ea6162953cff25d634f67caea325509557054e7ad1fd"
 
   def install
     bin.install "gocd"
