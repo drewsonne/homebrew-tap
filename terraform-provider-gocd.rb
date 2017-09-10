a34a0a058d815c4878794cc03c7ecc50d5f37fd1
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-10 21:51:13 +0100

    terraform-provider-gocd version 0.1.0

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,12 +1,9 @@
 class TerraformProviderGocd < Formula
-  desc "Terraform Provider to manage resources in GoCD Server"
+  desc ""
   homepage "https://github.com/drewsonne/terraform-provider-gocd"
-  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.0.11-rc2/terraform-provider-gocd-0.0.11-rc2-darwin-amd64.zip"
-  version "0.0.11-rc4"
-  sha256 "4490b7de3a538e613edfe9fe10dbba63dfb1d7930d7a42c36fd7ff34b3c02269"
-
-  depends_on "drewsonne/devops/tf-install-provider"
-  depends_on "terraform"
+  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.1.0/terraform-provider-gocd-0.1.0-darwin-amd64.zip"
+  version "0.1.0"
+  sha256 "781e44f94526e87a1582dde0553e167f2b7321fd9e82e1cbae5d0e9efbdf3cbd"
 
   def install
     bin.install "terraform-provider-gocd"
@@ -15,12 +12,4 @@ class TerraformProviderGocd < Formula
   test do
     system "#{bin}/terraform-provider-gocd; [[ $? -eq 1 ]]"
   end
-
-  def caveats
-    """
-After installing, you will need to copy the provider to a place terraform can find it by running:
-  tf-install-provider gocd
-"""
-  end
-
 end
