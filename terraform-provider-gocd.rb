de0aa9f275f5711ea2929918db7ba3fbd0ef179f
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-11 08:29:17 +0100

    terraform-provider-gocd version 0.1.1

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,9 +1,9 @@
 class TerraformProviderGocd < Formula
   desc ""
   homepage "https://github.com/drewsonne/terraform-provider-gocd"
-  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.1.0/terraform-provider-gocd-0.1.0-darwin-amd64.zip"
-  version "0.1.0"
-  sha256 "781e44f94526e87a1582dde0553e167f2b7321fd9e82e1cbae5d0e9efbdf3cbd"
+  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.1.1/terraform-provider-gocd-0.1.1-darwin-amd64.zip"
+  version "0.1.1"
+  sha256 "271e164f21b5792ad27efa3d784ddceb48991acd360c7f7b68072420cb763af2"
 
   def install
     bin.install "terraform-provider-gocd"
