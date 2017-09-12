3471b585effcde3285098e90fba70cb6f7804944
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-12 07:45:52 +0100

    gocd-tf-generate version 0.1.8

--- gocd-tf-generate.rb
+++ gocd-tf-generate.rb
@@ -1,9 +1,9 @@
 class GocdTfGenerate < Formula
   desc ""
   homepage "https://github.com/drewsonne/gocd-tf-generate"
-  url "https://github.com/drewsonne/gocd-tf-generate/releases/download/0.1.7/gocd-tf-generate-0.1.7-darwin-x86_64.zip"
-  version "0.1.7"
-  sha256 "7f8f7bf929378228ed12de7aa65c8cf6acec385ef87371c40ca775f3d0251ce9"
+  url "https://github.com/drewsonne/gocd-tf-generate/releases/download/0.1.8/gocd-tf-generate-0.1.8-darwin-x86_64.zip"
+  version "0.1.8"
+  sha256 "603a7c621dfc44dfbda51d8954e1a78fbcf3dcb02494ed556d4da3048365c67c"
 
   depends_on "drewsonne/tap/go-gocd"
 
