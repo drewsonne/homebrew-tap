ba0b7f9efc9583311d111fb8d34375bdeb342498
Drew J. Sonne <drew.sonne@gmail.com>
2017-10-27 07:40:12 +0100

    Updated tf-install to not require coreutils

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -1,7 +1,7 @@
 class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
   homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
-  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/de33c821535be3ac0b82c7b795314f314abdc1d8.zip"
+  url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/bc767eb9bc78772ac38cbedc290c5a89bb2df667.zip"
   version "1.0.8"
   sha256 "36241e7b020b947347392826732750243a6ccf8ddb975bc1615488d17a62836a"
 
