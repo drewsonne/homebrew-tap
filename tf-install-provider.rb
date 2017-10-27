9396fab1ff011ed49698b359586f204ac9915a26
Drew J. Sonne <drew.sonne@gmail.com>
2017-10-27 07:09:40 +0100

    Added coreutils dependency and version bump

--- tf-install-provider.rb
+++ tf-install-provider.rb
@@ -2,8 +2,10 @@ class TfInstallProvider < Formula
   desc "Helper utility to install terraform providers"
   homepage "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86"
   url "https://gist.github.com/drewsonne/693feb5278af158690cf65b1cb7d0e86/archive/de33c821535be3ac0b82c7b795314f314abdc1d8.zip"
-  version "1.0.7"
+  version "1.0.8"
   sha256 "36241e7b020b947347392826732750243a6ccf8ddb975bc1615488d17a62836a"
+
+  depends_on "coreutils"
 
   def install
     bin.install "tf-install-provider"
