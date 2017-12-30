7ec93bbd3fbbe212f55c7d153704f4c4aa03a85e
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-30 15:28:30 +0000

    go-gocd version 0.6.8

--- go-gocd.rb
+++ go-gocd.rb
@@ -1,9 +1,9 @@
 class GoGocd < Formula
   desc "Terraform Provider for ThoughtWorks GoCD"
   homepage "https://github.com/drewsonne/terraform-provider-gocd"
-  url "https://github.com/drewsonne/go-gocd/releases/download/0.6.6/gocd-0.6.6-darwin-x86_64.zip"
-  version "0.6.6"
-  sha256 "8f92fc9ccee87da4021df0603bfccd05b3e41e43d9f0bec7314ed662ca36c567"
+  url "https://github.com/drewsonne/go-gocd/releases/download/0.6.8/gocd-0.6.8-darwin-x86_64.zip"
+  version "0.6.8"
+  sha256 "40da76c30a8dbdad08b57b5f35a4ff975c3cb0f842e2337969b1cbd4123eaccc"
   
   depends_on "drewsonne/homebrew-tap/tf-install-provider"
 
