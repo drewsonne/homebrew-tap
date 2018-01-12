0f9844e06ddfd1ed6f20405e1979ccb5a6865e70
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-12 07:19:01 +0000

    terraform-provider-gocd version 0.1.19

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,12 +1,11 @@
 class TerraformProviderGocd < Formula
   desc "Terraform Provider for ThoughtWorks GoCD"
   homepage "https://github.com/drewsonne/terraform-provider-gocd"
-  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.1.17/terraform-provider-gocd-0.1.17-darwin-amd64.zip"
-  version "0.1.17"
-  sha256 "97568e247d7c06408cb195c24a7aadae9353de070376063225f00fbc19d55b49"
-  
-  depends_on "terraform"
-  depends_on "drewsonne/homebrew-tap/tf-install-provider"
+  url "https://github.com/drewsonne/terraform-provider-gocd/releases/download/0.1.19/terraform-provider-gocd-0.1.19-darwin-amd64.zip"
+  version "0.1.19"
+  sha256 "e60bed8550eebe0f243aa85b03d90136c326e11f88dcd2e7b821cec7343ea03a"
+
+  depends_on "terraform"depends_on "drewsonne/homebrew-tap/tf-install-provider"
 
   def install
     bin.install "terraform-provider-gocd"
